a0eeb3ee02e7ae321bb5e0a37ed92b01b7ef0a12
Fabio Hirata <fabiohh@yahoo-inc.com>
2016-09-18 19:23:31 -0700

    Including unit tests for sqlite.

---
+++ app/src/main/java/com/example/fabiohh/sunshine/app/data/LocationEntry.java
@@ -0,0 +1,6 @@
+package com.example.fabiohh.sunshine.app.data;
+
+/**
+ * Created by fabiohh on 9/4/16.
+ */
+
---
+++ app/src/main/java/com/example/fabiohh/sunshine/app/data/WeatherEntry.java
@@ -0,0 +1,9 @@
+package com.example.fabiohh.sunshine.app.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by fabiohh on 9/3/16.
+ */
