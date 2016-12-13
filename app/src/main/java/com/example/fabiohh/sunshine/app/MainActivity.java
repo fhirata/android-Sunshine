1463eadb2e26cbe7246cb482eae74eeff3dc79a9
Fabio Hirata <fabiohh@yahoo-inc.com>
2016-12-12 20:34:43 -0800

    Fixed portrait/landscape mode for phone.

--- app/src/main/java/com/example/fabiohh/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/fabiohh/sunshine/app/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.fabiohh.sunshine.app.sync.SunshineSyncAdapter;
@@ -54,7 +56,9 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
 
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
-        forecastFragment.setUseTodayLayout(!mTwoPane);
+        if (null != forecastFragment) {
+            forecastFragment.setUseTodayLayout(!mTwoPane);
+        }
 
         SunshineSyncAdapter.initializeSyncAdapter(this);
 
@@ -62,6 +66,28 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             Toast.makeText(this, "This device is not supported.", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private boolean checkPlayServices() {
