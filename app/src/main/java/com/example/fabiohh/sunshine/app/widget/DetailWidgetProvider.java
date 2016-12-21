3e923e27eab58a65201067b0c0f0b0c5d001cb5a
Fabio Hirata <fabio.hirata@gmail.com>
2016-12-20 18:13:41 -0800

    WIP for detail widget.

---
+++ app/src/main/java/com/example/fabiohh/sunshine/app/widget/DetailWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.example.fabiohh.sunshine.app.widget;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by fabiohh on 12/20/16.
+ */
+
+public class DetailWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
---
+++ app/src/main/java/com/example/fabiohh/sunshine/app/widget/DetailWidgetRemoteViewsService.java
@@ -0,0 +1,15 @@
+package com.example.fabiohh.sunshine.app.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by fabiohh on 12/20/16.
+ */
+
+public class DetailWidgetRemoteViewsService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}
